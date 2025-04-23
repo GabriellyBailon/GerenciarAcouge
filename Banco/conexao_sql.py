959ee2fc47eec9c6473d1d068dd62c4fc6b3d46a
Gabrielly Bailon <gabriellybailontur@gmail.com>
2025-04-22 23:47:11 -0300

    Ajustes tirando parte do banco que ainda não vai subir.

--- Banco/conexao_sql.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def conectar():
-    return mysql.connector.connect(
-        host="localhost",     # ou IP da sua máquina
-        user="root",          # ou outro usuário
-        password="sua_senha",
-        database="streamlit_app"
-    )
