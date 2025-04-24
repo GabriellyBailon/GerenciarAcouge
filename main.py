2b161fcdde6f17b32ac35624f6db9cbfa39c7511
Gabrielly Bailon <gabriellybailontur@gmail.com>
2025-04-23 21:24:31 -0300

    Mudando o nome do site

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import streamlit as st
 import calculadora_acougue
 
 st.set_page_config(
-    page_title="Açougue Digital",
+    page_title="Meu Açougue Online",
     page_icon="👋",
 )
 
-st.title("Bem vindo ao Açougue Digital! 👋")
+st.title("Bem vindo ao Meu Açougue Online! 👋")
 
 calculadora_acougue.CalcularMargem()
