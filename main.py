9b6711dc0de37791af83bf8ea2c71e4c76acbd6b
Gabrielly Bailon <gabriellybailontur@gmail.com>
2025-06-05 11:24:52 -0300

    Ajuste sem login.

--- main.py
+++ main.py
@@ -9,25 +9,7 @@ st.set_page_config(
 
 st.title("Bem vindo ao Meu Açougue Online! 👋")
 
-if not ms.is_logged_in():
-        ms.Login()  # Exibe a tela de login
-else:
-    st.sidebar.success(f"Logado como: {st.session_state['user']}")
+escolha = st.sidebar.radio("Menu", ["Calculadora"])
 
-    admin = ms.is_admin(st.session_state['user'])
-
-    if admin:
-        escolha = st.sidebar.radio("Menu", ["Calculadora", "Cadastrar Usuário", "Sair"])
-    else:
-        escolha = st.sidebar.radio("Menu", ["Calculadora", "Sair"])
-
-    if escolha == "Calculadora":
-        calculadora_acougue.CalcularMargem()
-
-    elif escolha == "Cadastrar Usuário" and admin:
-        ms.register_user()
-
-    elif escolha == "Sair":
-        st.session_state.clear()
-        st.success("Você saiu com sucesso.")
-        st.rerun()
+if escolha == "Calculadora":
+    calculadora_acougue.CalcularMargem()
